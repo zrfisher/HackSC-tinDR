e11a441e17b89c97b05836cdda2eda0f7555c095
Zoe Fisher <zrfisher@usc.edu>
2020-02-01 05:04:27 -0800

    created new variables

--- app/src/main/java/com/example/tindr/SwipeyActivity.java
+++ app/src/main/java/com/example/tindr/SwipeyActivity.java
@@ -42,8 +42,36 @@ public class SwipeyActivity extends AppCompatActivity {
     private void loadNextDr(){
         if(count >= data.getData().length){
             Toast.makeText(getApplicationContext(), "Sorry, no more doctors available", Toast.LENGTH_LONG).show();
-        } else {
-            textView.setText(data.getData()[count].getProfile().getFirst_name());
+            return;
         }
+
+        Profile profile = data.getData()[count].getProfile();
+        String firstName = profile.getFirst_name();
+        String lastName = profile.getLast_name();
+        String image_url = profile.getLast_name();
+        String gender = profile.getGender();
+        String bio = profile.getBio();
+        Language[] l = profile.getLanguages();
+        String[] languages = new String[l.length];
+        for(int i = 0; i<languages.length; i++){
+            languages[i] = l[i].getName();
+        }
+
+        Specialty[] sps = data.getData()[count].getSpecialties();
+        String[] specialties = new String[sps.length];
+        for(int i = 0; i<specialties.length; i++){
+            specialties[i] = sps[i].getDescription();
+        }
+
+        Practice practice = data.getData()[count].getPractices()[0];
+        double lat = practice.getLat();
+        double lon = practice.getLon();
+        Phone[] phones = practice.getPhones();
+        String[] phoneNums = new String[phones.length];
+        for(int i = 0; i<phoneNums.length; i++){
+            phoneNums[i] = phones[i].getNumber();
+        }
+
+        textView.setText(firstName);
     }
 }
